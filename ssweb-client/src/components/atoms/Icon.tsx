592c087c2d64da863b3ff4a75e8ad54b7e0b2dc2
reisyun <reisyun7@gmail.com>
2021-01-06 07:26:24 +0900

    feat: Add Icon component

---
+++ ssweb-client/src/components/atoms/Icon.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { size as iconSize } from 'polished';
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import { ThemeProps } from '@styles/theme';
+import { IconSizeType } from '@styles/theme/size';
+import { ColorType } from '@styles/theme/palette';
+import * as icons from '@assets/icons';
+
+type Color = keyof ColorType;
+type Size = keyof IconSizeType;
+
+interface StyledIconProps {
+  color?: Color;
+  size?: Size;
+}
+
+const baseStyles = css`
+  display: inline-flex;
+`;
+
+const colorStyles = ({ theme, color = 'secondary' }: StyledIconProps & ThemeProps) =>
+  color &&
+  css`
+    & > svg {
+      fill: ${theme.palette.color[color]};
+    }
+  `;
+
+const sizeStyles = ({ theme, size = 'large' }: StyledIconProps & ThemeProps) =>
+  size &&
+  css`
+    & > svg {
+      ${iconSize(theme.size.iconSize[size])};
+    }
+  `;
+
+const StyledIcon: React.FC<StyledIconProps> = styled.i<StyledIconProps>(
+  baseStyles,
+  colorStyles,
+  sizeStyles,
+);
+
+/**
+ * Icon component
+ */
+type Icons = keyof typeof icons;
+
+export interface IconProps extends StyledIconProps {
+  icon: Icons;
+}
+
+function Icon({ icon, ...rest }: IconProps) {
+  const SVG = icons[icon];
+
+  return (
+    <StyledIcon {...rest}>
+      <SVG />
+    </StyledIcon>
+  );
+}
+
+Icon.defaultProps = {
+  size: 'large',
+  color: 'secondary',
+};
+
+export default Icon;
