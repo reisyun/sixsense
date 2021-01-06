e091b2599d361a9360d6ffbc0e13556253b90d01
reisyun <reisyun7@gmail.com>
2021-01-06 09:02:01 +0900

    feat: Add Divider component

---
+++ ssweb-client/src/components/atoms/Divider.tsx
@@ -0,0 +1,31 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import { ThemeProps } from '@/styles/theme';
+
+export interface DividerProps {
+  vertical?: boolean;
+}
+
+const baseStyles = ({ theme, vertical = false }: DividerProps & ThemeProps) => {
+  const divider = {
+    horizon: css`
+      margin: 0;
+      width: 100%;
+      border-color: ${theme.palette.background.divider};
+    `,
+    vertical: css`
+      display: inline-block;
+      margin: 0 8px;
+      width: 1px;
+      height: 1em;
+      vertical-align: middle;
+      border-color: ${theme.palette.background.divider};
+    `,
+  };
+
+  return vertical ? divider.vertical : divider.horizon;
+};
+
+const Divider = styled.hr<DividerProps>(baseStyles);
+
+export default Divider;
